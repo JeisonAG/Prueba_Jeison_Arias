8c896b6f2371e6772cd9b6c9fc1ab9efcfadaa58
JeisonAG <106494309+JeisonAG@users.noreply.github.com>
2022-05-29 15:53:55 -0600

    Add files via upload

--- Prueba_Api/Data/Connection.cs
+++ Prueba_Api/Data/Connection.cs
@@ -2,6 +2,6 @@
 {
     public class Connection
     {
-        public static string connectionString = "Data Source=172.16.165.20\\SQL2016Dev;Initial Catalog=PRUEBA_DB;Integrated Security=false;Persist Security Info=True;User ID=AccesoOBS;Password=Baxter123#;";
+        public static string connectionString = "Data Source=xxxxx;Initial Catalog=PRUEBA_DB;Integrated Security=false;Persist Security Info=True;User ID=xxxx;Password=xxx;"; //Cambiar credenciales
     }
 }
